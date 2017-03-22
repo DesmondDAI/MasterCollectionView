1ec4f9a390615c0f8fa408a77d72b4c59aad5c89
DesmondDAI <dxmlore@gmail.com>
2017-03-22 20:47:02 +0800

    [New] Add Cocoapods support and .gitignore file

--- MasterCollectionView/ViewController.swift
+++ MasterCollectionView/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.myCollectionView.reloadItems(at: [IndexPath()])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
